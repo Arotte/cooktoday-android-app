b079730ccfd8bd32e2676f2b7097eccdee30fc41
Aron Molnar <thearote@gmail.com>
2022-02-24 13:41:51 +0000

    update first page of onboarding survey

--- app/src/main/java/com/aron/cooktoday/onboarding/survey/steps/SurveyFragment1.java
+++ app/src/main/java/com/aron/cooktoday/onboarding/survey/steps/SurveyFragment1.java
@@ -3,17 +3,27 @@ package com.aron.cooktoday.onboarding.survey.steps;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.aron.cooktoday.R;
+import com.aron.cooktoday.onboarding.survey.rvadapters.CuisinesRecyclerViewAdapter;
 
-public class SurveyFragment1 extends Fragment {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SurveyFragment1 extends Fragment implements CuisinesRecyclerViewAdapter.ItemClickListener {
+
+    CuisinesRecyclerViewAdapter rvAdapterCuisines;
+    List<String> cuisineNames;
 
     public SurveyFragment1() {
-        // required empty public constructor
     }
 
     public static SurveyFragment1 newInstance() {
@@ -29,12 +39,29 @@ public class SurveyFragment1 extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View layout = inflater.inflate(R.layout.fragment_survey_step1, container, false);
 
+        initCuisinesRecyclerView(layout);
         return layout;
     }
 
+    @Override
+    public void onCuisineItemClick(View view, int position) {
+        // TODO
+    }
+
+    private void initCuisinesRecyclerView(View layout) {
+        RecyclerView rvContainer = layout.findViewById(R.id.rvSurveyStep1Cuisines);
+
+        // fill up cuisine names list
+        // TODO: get it from server + get cuisine images from server
+        cuisineNames = new ArrayList<>();
+        cuisineNames = Arrays.asList("AMERICAN", "MEXICAN", "FRENCH", "ITALIAN", "ASIAN", "BBQ", "KID-FRIENDLY", "GREEK", "ENGLISH", "THAI", "GERMAN", "IRISH", "INDIAN", "CUBAN", "SWEDISH", "HUNGARIAN");
+
+        rvContainer.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        rvAdapterCuisines = new CuisinesRecyclerViewAdapter(getActivity(), cuisineNames);
+        rvAdapterCuisines.setClickListener(this);
+        rvContainer.setAdapter(rvAdapterCuisines);
+    }
 }
